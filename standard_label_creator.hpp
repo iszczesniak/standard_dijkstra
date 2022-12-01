ae79e6a03d7ad8b3139c4a9ca82805907e816b3a
Irek Szcześniak <irek.szczesniak@gmail.com>
2022-12-01 12:27:11 +0100

    now we use the props.hpp

--- standard_label_creator.hpp
+++ standard_label_creator.hpp
@@ -1,36 +1,24 @@
 #ifndef STANDARD_LABEL_CREATOR_HPP
 #define STANDARD_LABEL_CREATOR_HPP
 
-#include "standard_label.hpp"
-
 #include <array>
-#include <tuple>
 
 // Creates new labels.
-template <typename Graph, typename Cost>
 struct standard_label_creator
 {
-  using Label = standard_label<Graph, Cost>;
-
-  const Graph &m_g;
-
-  standard_label_creator(const Graph &g):
-    m_g(g)
-  {
-  }
-
   // Create a new label: at the target node of label l, take edge e.
-  template <Callable<Cost> F = EmptyCallable<Cost>>
+  template <typename Edge, typename Label,
+            Callable<Label> F = EmptyCallable<Label>>
   auto
-  operator()(const Edge<Graph> &e, const Label &l, F f = {}) const
+  operator()(const Edge &e, const Label &l, F f = {}) const
   {
-    // The cost of the new label = cost of label l + cost of edge e.
-    Cost c = get_cost(l) + boost::get(boost::edge_weight, m_g, e);
+    // Weight of the new label = weight of label l + weight of edge e.
+    auto w = get_weight(e) + get_weight(l)
 
     f(c);
 
     // At cost c, and along edge e, we reach the target of e.
-    return std::array<Label, 1>({Label(c, e, target(e, m_g))});
+    return std::array<Label, 1>({Label(e, w)});
   }
 };
 
