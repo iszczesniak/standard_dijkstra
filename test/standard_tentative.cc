ef1b819b33ce551a0ffa82c5d45ace5d7b94a698
Irek Szcześniak <irek.szczesniak@gmail.com>
2022-12-07 12:55:06 +0100

    wip operator <

--- test/standard_tentative.cc
+++ test/standard_tentative.cc
@@ -6,19 +6,41 @@ struct label: weight<unsigned>, index<unsigned>
   label(unsigned w, unsigned i): weight(w), index(i)
   {
   }
-};
 
-bool
-operator < (const label &l1, const label &l2)
-{
-  return get_weight(l1) < get_weight(l2);
-}
+  constexpr auto
+  operator <=> (const label &l) const
+  {
+    if (get_weight(*this) < get_weight(l))
+      return std::strong_ordering::less;
+    if (get_weight(*this) > get_weight(l))
+      return std::strong_ordering::greater;
+
+    return std::strong_ordering::equal;
+  }
+
+  bool operator == (const label &) const = default;
+};
 
 int main()
 {
   label l1(1, 0), l2(0, 0), l3(0, 1);
   standard_tentative<label> t(2);
   t.push(l1);
-  t.push(l2);
   t.push(l3);
+
+  {
+    auto l = t.pop();
+    assert(l == l3);
+  }
+  
+  {
+    auto l = t.pop();
+    assert(l == l1);
+  }
+
+  t.push(l2);
+  {
+    auto l = t.pop();
+    assert(l == l2);
+  }
 }
